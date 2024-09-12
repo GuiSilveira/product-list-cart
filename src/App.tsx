8553b6d840c0db256bcedaae0fd774bef93a0641
Guilherme Silveira <guisilveira.cout@gmail.com>
2024-09-11 23:41:12 -0300

    estrutura de card criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react'
+import Card from './components/Card/Card'
+
 function App() {
+    const [cart, setCart] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('../data.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                setData(data)
+            })
+    }, [])
+
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
-        </div>
+        <main className="m-6">
+            <h1 className="text-3xl font-bold mb-8">Desserts</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                {data.map((item, index) => (
+                    <Card key={index} item={item} />
+                ))}
+            </div>
+        </main>
     )
 }
 
