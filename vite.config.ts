ac01d9f5da539c1526999f0820a6c0e5ea048c43
Guilherme Silveira <guisilveira.cout@gmail.com>
2024-09-13 21:35:02 -0300

    estrutura de deploy criada

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/product-list-cart/',
     plugins: [
         react(),
         svgr({
